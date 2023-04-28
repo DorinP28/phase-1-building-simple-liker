9d093bcf4bb2da85a1c0211c5d6ebb77a61bcc94
Dorin Porcereanu <porcereanu.dorin@gmail.com>
2023-04-27 21:39:52 -0400

    done

--- main.js
+++ main.js
@@ -3,7 +3,30 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const exampleHeart = document.getElementsByClassName("like-glyph");
+console.log(exampleHeart)
 
+for(let heart of exampleHeart){
+  console.log(heart)
+  heart.addEventListener('click', () => {
+    mimicServerCall()
+    .then(() => {
+      if(heart.className === 'activated-heart'){
+        heart.textContent = EMPTY_HEART
+        heart.classList.remove('activated-heart')
+      }else{
+        heart.textContent = FULL_HEART
+        heart.className = "activated-heart"
+      }
+    })
+    .catch((errorMessage)=>{
+      const error = document.getElementById('modal')
+      error.classList.remove('hidden')
+      error.textContent = errorMessage;
+      setTimeout(() => error.classList.add('hidden'), 3000)
+    })
+  })
+}
 
 
 
